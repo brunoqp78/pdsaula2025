75ae5647bcee7e6988f753d0938fffc883acf4f8
brunoqp78 <bruno.queiroz@iftm.edu.br>
2025-04-29 09:46:24 -0300

    aula dia 22 de abril

--- src/main/java/org/iftm/projetoaula/ProjetoaulaApplication.java
+++ src/main/java/org/iftm/projetoaula/ProjetoaulaApplication.java
@@ -1,6 +1,7 @@
 package org.iftm.projetoaula;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.iftm.projetoaula.entities.Client;
@@ -16,8 +17,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 */
 @SpringBootApplication
 public class ProjetoaulaApplication implements CommandLineRunner {
-	//injeção de dependencia da classe ClientRepository.
-	//permite ao SpringBoot instanciar objetos dessa classe.
+	// injeção de dependencia da classe ClientRepository.
+	// permite ao SpringBoot instanciar objetos dessa classe.
 	@Autowired
 	private ClientRepository repositorio;
 
@@ -27,25 +28,24 @@ public class ProjetoaulaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
 
-		//Instanciar um objeto da classe Client
-		Client cliente = new Client();		
+		// Instanciar um objeto da classe Client
+		Client cliente = new Client();
 		cliente.setName("Ana");
 		cliente.setCpf("000111222-22");
 		cliente.setIncome(10000.00);
 		cliente.setChildren(3);
-		cliente.setBirthDate(Instant.parse("1978-10-09T04:30:00.00Z"));		
-		repositorio.save(cliente);		
+		cliente.setBirthDate(Instant.parse("1978-10-09T04:30:00.00Z"));
+		repositorio.save(cliente);
 
-		Client cliente2 = new Client(null, "Jose","11111", 12000.00, Instant.parse("1978-10-09T04:30:00.00Z"), 2);
+		Client cliente2 = new Client(null, "Jose", "11111", 12000.00, Instant.parse("1978-10-09T04:30:00.00Z"), 2);
 		repositorio.save(cliente2);
 
 		cliente2.setName("Maria");
 		repositorio.save(cliente2);
 
-		//repositorio.deleteAll();
-		//repositorio.deleteById(2L);
+		// repositorio.deleteAll();
+		// repositorio.deleteById(2L);
 		List<Client> clientes = repositorio.findAll();
 		System.out.println("Relatório:::::");
 		for (int i = 0; i < clientes.size(); i++) {
@@ -58,6 +58,86 @@ public class ProjetoaulaApplication implements CommandLineRunner {
 		Client busca = repositorio.findById(2L).get();
 		System.out.println("Busca individual:");
 		System.out.println(busca.getName());
+
+		// Query Methods
+		// find by atributos - similar ao select de BD
+		// queryMethods retorna uma lista de clientes
+		// findBy indica que é uma consulta, children indica qual campo será buscado.
+		ArrayList<Client> listaClientes = repositorio.findByChildren(2);
+		System.out.println("-------- Saída ----------");
+		System.out.println(listaClientes.size());// imprimir quantidade de clientes com dois filhos
+		System.out.println("Relatório:::::");
+		for (int i = 0; i < listaClientes.size(); i++) {
+			System.out.println(listaClientes.get(i).getName());
+		}
+		System.out.println("-------- Saída ----------");
+
+		// findBy indica que é uma consulta, children indica qual campo será buscado.
+		listaClientes = repositorio.findByName("João Silva");
+		System.out.println("-------- Saída Name ----------");
+		System.out.println(listaClientes.size());// imprimir quantidade de clientes com dois filhos
+		System.out.println("Relatório:::::");
+		for (int i = 0; i < listaClientes.size(); i++) {
+			System.out.println(listaClientes.get(i).getName());
+		}
+		System.out.println("-------- Saída ----------");
+
+		// findBy indica que é uma consulta, name e children indica quais campo serão
+		// buscados.
+		listaClientes = repositorio.findByNameOrChildren("João Silva", 0);
+		System.out.println("-------- Saída Name e Children ----------");
+		System.out.println(listaClientes.size());// imprimir quantidade de clientes com dois filhos
+		System.out.println("Relatório:::::");
+		for (int i = 0; i < listaClientes.size(); i++) {
+			System.out.println(listaClientes.get(i).getName());
+		}
+		System.out.println("-------- Saída ----------");
+
+		listaClientes = repositorio.findByChildrenGreaterThanEqual(2);
+		System.out.println("-------- Saída Quantidade de filhos ----------");
+		System.out.println(listaClientes.size());// imprimir quantidade de clientes com dois filhos
+		System.out.println("Relatório:::::");
+		for (int i = 0; i < listaClientes.size(); i++) {
+			System.out.println(listaClientes.get(i).getName() + " - " + listaClientes.get(i).getChildren());
+		}
+		System.out.println("-------- Saída ----------");
+
+		System.out.println("-------- Saída ----------");
+
+		listaClientes = repositorio.findByChildrenBetween(1, 3);
+		System.out.println("-------- Saída Quantidade de filhos ----------");
+		System.out.println(listaClientes.size());// imprimir quantidade de clientes com dois filhos
+		System.out.println("Relatório:::::");
+		for (int i = 0; i < listaClientes.size(); i++) {
+			System.out.println(listaClientes.get(i).getName() + " - " + listaClientes.get(i).getChildren());
+		}
+		System.out.println("-------- Saída ----------");
+
+		System.out.println("-------- Saída ----------");
+
+		listaClientes = repositorio.findByNameContainingOrderByChildrenDesc("Maria");
+		System.out.println("-------- Saída Quantidade de filhos ----------");
+		System.out.println(listaClientes.size());// imprimir quantidade de clientes com dois filhos
+		System.out.println("Relatório:::::");
+		for (int i = 0; i < listaClientes.size(); i++) {
+			System.out.println(listaClientes.get(i).getName() + " - " + listaClientes.get(i).getChildren());
+		}
+		System.out.println("-------- Saída ----------");
+
+		System.out.println("-------- Saída ----------");
+		ArrayList<Integer> idades = new ArrayList<>();
+		idades.add(1);
+		idades.add(3);
+		listaClientes = repositorio.findByChildrenIn(idades);
+		System.out.println("-------- Saída Quantidade de filhos ----------");
+		System.out.println(listaClientes.size());// imprimir quantidade de clientes com dois filhos
+		System.out.println("Relatório:::::");
+		for (int i = 0; i < listaClientes.size(); i++) {
+			System.out.println(listaClientes.get(i).getName() + " - " + listaClientes.get(i).getChildren());
+		}
+		System.out.println("-------- Saída ----------");
+
+
 	}
 
 }
--- src/main/java/org/iftm/projetoaula/repositories/ClientRepository.java
+++ src/main/java/org/iftm/projetoaula/repositories/ClientRepository.java
@@ -1,5 +1,7 @@
 package org.iftm.projetoaula.repositories;
 
+import java.util.ArrayList;
+
 import org.iftm.projetoaula.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +9,31 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long>{
+
+    public ArrayList<Client> findByChildren(int children);
+
+    public ArrayList<Client> findByName(String name);
+
+    public ArrayList<Client> findByNameAndChildren(String string, int i);
+
+    public ArrayList<Client> findByNameOrChildren(String string, int i);
+
+    public ArrayList<Client> findDistinctByChildren(Integer children);
+
+    public ArrayList<Client> findByChildrenGreaterThan(int i);
+
+    public ArrayList<Client> findByChildrenGreaterThanEqual(int i);
+
+    public ArrayList<Client> findByChildrenBetween(int i, int j);
+
+    public ArrayList<Client> findByNameLike(String string);
+
+    public ArrayList<Client> findByNameContaining(String string);
+
+    public ArrayList<Client> findByNameContainingOrderByChildren(String string);
+
+    public ArrayList<Client> findByNameContainingOrderByChildrenDesc(String string);
+
+    public ArrayList<Client> findByChildrenIn(ArrayList<Integer> idades);
+    
 }
